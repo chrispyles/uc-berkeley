cb788850e9ec2fb6f2cf20241105a158064ce688
Chris Pyles <cpyles@berkeley.edu>
2019-04-04 20:55:27 -0700

    testing markdown

---
+++ header.js
@@ -0,0 +1,12 @@
+var html = `<div class="fixed">
+
+			<h1 class="name"><a id="name" href="https://cpyles.com/index.html">Christopher Pyles</a></h1>
+
+				<p></p>
+
+				<p class="links"><a href="https://cpyles.com/index.html">About Me</a> | <a href="https://cpyles.com/education.html">Education</a> | <a href="https://cpyles.com/interests.html">Interests</a> | <a href="./index.html">Course Development</a> | <a href="https://cpyles.com/co-curriculars.html">Co-Curriculars</a> | <a href="https://cpyles.com/experience.html">Work Experience</a> | <a href="https://cpyles.com/index.html#resume">Resume</a> | <a href="https://jupyter.cpyles.com" target="_blank">Jupyter</a></p>
+
+		</div>
+`;
+
+document.write(html);
